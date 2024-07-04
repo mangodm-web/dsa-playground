987c6642549acf6bc7b15b1b7d89e4bf8cec762a
Dami Huh <123475341+mangodm-web@users.noreply.github.com>
2024-07-04 23:13:40 +0900

    Time: 490 ms (67.54%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+SELECT
+    ROUND(AVG(CASE 
+                  WHEN order_date = customer_pref_delivery_date THEN 1 
+                  ELSE 0 
+              END) * 100.0, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
