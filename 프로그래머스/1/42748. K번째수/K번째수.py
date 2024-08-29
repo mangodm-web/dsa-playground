97083996c943cf50464d7817eecbb9dd249601a2
Dami Huh <123475341+mangodm-web@users.noreply.github.com>
2024-08-29 21:46:06 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands) -> int:
+    answer = []
+    
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i-1:j])[k-1])
+
+    return answer
