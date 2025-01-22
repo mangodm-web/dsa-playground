d2a433e56062a0f3960a47c9095b4eb5831dc8e8
Dami Huh <123475341+mangodm-web@users.noreply.github.com>
2025-01-22 20:06:56 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,49 +1,19 @@
-from typing import List
+from collections import deque
+import math
 
 
-def solution(progresses: List[int], speeds: List[int]) -> List[int]:
-    """
-    각 작업의 진도와 작업 속도를 바탕으로 매 배포마다 완료된 기능의 개수를 계산하여 반환한다.
-
-    Args:
-        progresses (List[int]): 각 작업의 현재 진행 상태를 나타내는 리스트 (100 미만 자연수)
-        speeds (List[int]): 각 작업의 속도를 나타내는 리스트 (100 이하의 자연수)
-
-    Returns:
-        List[int]: 각 배포마다 몇 개의 기능이 배포되는지가 담긴 리스트
-
-    Notes:
-        idea:
-            1. 각 작업의 진도와 작업 속도를 하나의 작업 단위로 묶어 큐에 저장한다.
-            2. 매일 모든 작업의 진행 상태를 업데이트하고, 첫번째 작업이 완료되었을 때 큐에서 제거한다.
-            - 이때, 그 뒤의 작업들도 같이 배포할 수 있는지 확인한다.
-            - 완료된 작업들은 큐에서 제거하며, 해당 배포에 반영할 수 있는 기능의 개수를 세어서 결과 리스트에 저장한다.
-            3. 큐 안의 모든 작업이 처리될 때까지 이 과정을 반복한다.
-        time complexity: O(n^2), n은 전체 작업의 수(progressses, speeds 배열의 길이).
-            매일 모든 작업의 진행 상태를 업데이트하고, 작업이 완료될 때마다 큐에서 제거하는 과정에서
-            이중 루프가 발생할 수 있다.
-        space complexity: O(n), n은 전체 작업의 수
-            큐와 결과 리스트에 최대 n개의 값이 저장될 수 있다.
-    """
+def solution(progresses, speeds):
     result = []
-    q = [
-        {"progress": progress, "speed": speed}
-        for progress, speed in zip(progresses, speeds)
-    ]
-
-    while q:
-        q = [
-            {"progress": elem["progress"] + elem["speed"], "speed": elem["speed"]}
-            for elem in q
-        ]
-
-        if q[0]["progress"] >= 100:
-            feature_count = 0
-
-            while q and q[0]["progress"] >= 100:
-                q.pop(0)
-                feature_count += 1
-
-            result.append(feature_count)
+    days_remaining = deque(math.ceil((100 - p) / s) for p, s in zip(progresses, speeds))
+    
+    while days_remaining:
+        current_day = days_remaining.popleft()
+        features_deployed = 1
+        
+        while days_remaining and days_remaining[0] <= current_day:
+            days_remaining.popleft()
+            features_deployed += 1
+            
+        result.append(features_deployed)
 
     return result
