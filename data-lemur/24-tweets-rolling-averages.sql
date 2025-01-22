b0dd4d17649c4b62e441bca12b0a13de8712e47e
mangodm-web <mangodm.web3@gmail.com>
2025-01-22 18:46:55 +0900

    Add DataLemur solution for problem 24

---
+++ data-lemur/24-tweets-rolling-averages.sql
@@ -0,0 +1,12 @@
+SELECT
+    user_id,
+    tweet_date,
+    ROUND(
+        AVG(tweet_count) OVER (
+            PARTITION BY user_id
+            ORDER BY tweet_date
+            ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+        ),
+        2
+    ) AS rolling_avg_3rd
+FROM tweets;
