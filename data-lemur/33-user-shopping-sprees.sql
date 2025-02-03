e9441b33a43a56106690f05246d248bf95fefc31
mangodm-web <mangodm.web3@gmail.com>
2025-02-04 00:06:16 +0900

    Update DataLemur solution for problem 33

--- data-lemur/33-user-shopping-sprees.sql
+++ data-lemur/33-user-shopping-sprees.sql
@@ -1,7 +1,9 @@
 SELECT DISTINCT t1.user_id
 FROM transactions AS t1
 INNER JOIN transactions AS t2
-  ON DATE(t2.transaction_date) = DATE(t1.transaction_date) + 1
+  ON t1.user_id = t2.user_id
+ AND DATE(t2.transaction_date) = DATE(t1.transaction_date) + 1
 INNER JOIN transactions AS t3
-  ON DATE(t3.transaction_date) = DATE(t1.transaction_date) + 2
+  ON t1.user_id = t3.user_id
+ AND DATE(t3.transaction_date) = DATE(t1.transaction_date) + 2
 ORDER BY t1.user_id;
