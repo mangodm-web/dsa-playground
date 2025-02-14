cefdf888f964ee85c07f66d32498303269059fa7
Dami Huh <123475341+mangodm-web@users.noreply.github.com>
2025-02-14 19:17:36 +0900

    [level 2] Title: 타겟 넘버, Time: 445.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,12 @@
-from typing import List
-
-def dfs(numbers: List[int], target: int, depth: int = 0, cum_sum: int = 0) -> int:
+def dfs(numbers, target, depth=0, cum_sum=0):
     if depth == len(numbers):
-        if cum_sum == target:
-            return 1
-        else:
-            return 0
-    
-    add_path = dfs(numbers, target, depth + 1, cum_sum + numbers[depth])
-    subtract_path = dfs(numbers, target, depth + 1, cum_sum - numbers[depth])
-    
-    return add_path + subtract_path
+        return int(cum_sum == target)
 
-def solution(numbers: List[int], target: int) -> int:
+    return (
+        dfs(numbers, target, depth + 1, cum_sum + numbers[depth])
+        + dfs(numbers, target, depth + 1, cum_sum - numbers[depth])
+    )
+
+
+def solution(numbers, target):
     return dfs(numbers, target)
